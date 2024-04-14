00cdcb69aa122392c7a44d707a1474631c80ae9e
AnikaEma5131 <anikajasimema@gmail.com>
2024-04-14 21:41:41 +0600

    ppp
    up

--- src/Ema/UploadTrailerSceneController.java
+++ src/Ema/UploadTrailerSceneController.java
@@ -13,15 +13,10 @@ import java.nio.file.Files;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
-import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -97,5 +92,4 @@ public class UploadTrailerSceneController implements Initializable {
         alert.setContentText(message);
         alert.showAndWait();
     }
-
 }
